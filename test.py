5111709020088eaef8ea3f2601d523facc217e53
Bharat Kumar <bharatkumar@Bharats.local>
2023-10-04 23:24:55 -0500

    concatenation and minor changes

--- test.py
+++ test.py
@@ -12,4 +12,5 @@ def client():
 def test_application_isworking(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello World!" in response.data 
+    assert b"Hello World!" in response.data
+    
